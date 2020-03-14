03f1e99463b56646e405681135dcef80e772f118
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 15:36:51 +0200

    Optimize

--- check-deps.plugin.zsh
+++ check-deps.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if (( $+functions[zpm] )); then
-  zpm zpm-zsh/colors,inline
+  zpm zpm-zsh/colors
 fi
 
 typeset -g DEPENDENCES_ARCH
@@ -9,12 +9,12 @@ typeset -g DEPENDENCES_DEBIAN
 typeset -g DEPENDENCES_NPM
 typeset -g DEPENDENCES_PIP
 
-function gen_install_msg(){
+function gen_install_msg() {
   install_message="$c[cyan]Please install missing packages using: $c[red]\`$c[yellow]$1$c[red]\`$c_reset"
   echo $install_message
 }
 
-function get_package_if_need(){
+function get_package_if_need() {
   local executable
   local package
   if [[ "$1" == *@* ]]; then
@@ -24,66 +24,72 @@ function get_package_if_need(){
     package="$1"
     executable="$1"
   fi
-  
+
   if (( ! $+commands[$executable] )); then
     echo $package
   fi
 }
 
-function Check-Deps(){
+function _check_deps() {
   local i
-  
+
   #Arch System Deps
-  if (( $+commands[pacman] )); then
-    local DEPENDENCES_ARCH_MISSING=()
-    for i ($DEPENDENCES_ARCH); do
-      DEPENDENCES_ARCH_MISSING+=( $(get_package_if_need $i) )
-    done
-    if [[ ! -z "$DEPENDENCES_ARCH_MISSING" ]]; then
-      gen_install_msg "sudo pacman -S $DEPENDENCES_ARCH_MISSING"
-    fi
-  fi
-  
+  ((if (( $+commands[pacman] )); then
+        local DEPENDENCES_ARCH_MISSING=()
+        for i ($DEPENDENCES_ARCH); do
+          DEPENDENCES_ARCH_MISSING+=( $(get_package_if_need $i) )
+        done
+        if [[ ! -z "$DEPENDENCES_ARCH_MISSING" ]]; then
+          gen_install_msg "sudo pacman -S $DEPENDENCES_ARCH_MISSING"
+        fi
+      fi
+  ) &)
+
   # Debian || Ubuntu
-  if (( $+commands[dpkg] )); then
-    local DEPENDENCES_DEBIAN_MISSING=()
-    for i ($DEPENDENCES_DEBIAN); do
-      DEPENDENCES_DEBIAN_MISSING+=( $(get_package_if_need $i) )
-    done
-    if [ ! -z "$DEPENDENCES_DEBIAN_MISSING" ]; then
-      gen_install_msg "sudo apt install $DEPENDENCES_DEBIAN_MISSING"
-    fi
-  fi
-  
+  ((if (( $+commands[dpkg] )); then
+        local DEPENDENCES_DEBIAN_MISSING=()
+        for i ($DEPENDENCES_DEBIAN); do
+          DEPENDENCES_DEBIAN_MISSING+=( $(get_package_if_need $i) )
+        done
+        if [ ! -z "$DEPENDENCES_DEBIAN_MISSING" ]; then
+          gen_install_msg "sudo apt install $DEPENDENCES_DEBIAN_MISSING"
+        fi
+      fi
+  ) &)
+
   # Node.js
-  if (( $+commands[npm] )); then
-    local DEPENDENCES_NPM_MISSING=()
-    for i ($DEPENDENCES_NPM); do
-      DEPENDENCES_NPM_MISSING+=( $(get_package_if_need $i) )
-    done
-    if [ ! -z "$DEPENDENCES_NPM_MISSING" ]; then
-      gen_install_msg "npm install -g $DEPENDENCES_NPM_MISSING"
-    fi
-  fi
-  
+  (( if (( $+commands[npm] )); then
+        local DEPENDENCES_NPM_MISSING=()
+        for i ($DEPENDENCES_NPM); do
+          DEPENDENCES_NPM_MISSING+=( $(get_package_if_need $i) )
+        done
+        if [ ! -z "$DEPENDENCES_NPM_MISSING" ]; then
+          gen_install_msg "npm install -g $DEPENDENCES_NPM_MISSING"
+        fi
+      fi
+  ) &)
+
   # PIP
-  if (( $+commands[pip] )); then
-    local DEPENDENCES_PIP_MISSING=()
-    for i ($DEPENDENCES_PIP); do
-      DEPENDENCES_PIP_MISSING+=( $(get_package_if_need $i) )
-    done
-    if [ ! -z "$DEPENDENCES_PIP_MISSING" ]; then
-      gen_install_msg "pip install --user $DEPENDENCES_PIP_MISSING"
-    fi
-  fi
-  
+  (( if (( $+commands[pip] )); then
+        local DEPENDENCES_PIP_MISSING=()
+        for i ($DEPENDENCES_PIP); do
+          DEPENDENCES_PIP_MISSING+=( $(get_package_if_need $i) )
+        done
+        if [ ! -z "$DEPENDENCES_PIP_MISSING" ]; then
+          gen_install_msg "pip install --user $DEPENDENCES_PIP_MISSING"
+        fi
+      fi
+  ) &)
+
 }
 
-function _check_deps(){
-  Check-Deps
-  precmd_functions=(${precmd_functions#_check_deps})
+function _check_deps_init() {
+  _check_deps
+  add-zsh-hook -d precmd _check_deps_init
 }
+
 
 if [[ "$CHECK_DEPS_AT_START" != "false" ]]; then
-  precmd_functions+=( _check_deps )
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd _check_deps_init
 fi
